2e147c477e7903a748945baa54deb454c0812025
Vasudevan Govardhanen <48339921+p1npo1nt@users.noreply.github.com>
2025-03-28 05:08:58 -0400

    Update myQueue.java

--- mp3/bfs/myQueue.java
+++ mp3/bfs/myQueue.java
@@ -1,41 +1,41 @@
 package bfs;
 
 public class myQueue {
-    // array based queue for characters
     
     public static final int max = 1000;
-    private int front;
-    private int size; // amount of elements in the queue
+    private char[] q;
+    private int front; 
+    private int rear;
     private int cap; // max size of the queue
+    private int size; // current size of the queue
     char[] a;
 
-    public myQueue() {
-        a = new char[max]; // creating a character array
+    public myQueue(int cap) {
+        this.cap = cap;
+        this.q = new char[cap]; // creating a character array
         this.front = 0;
+        this.rear = 0;
         this.size = 0;
-        this.cap = max;
     }
 
     // enqueue
     public void eq(char s) {
         if(size==cap) {throw new IllegalArgumentException("Queue is full");}
-        int rear = (front+size)%cap;
-        a[rear] = s;
+        q[rear] = s;
+        rear = (rear + 1) % cap; // circular increment
         size+=1;
     }
 
     // dequeue
     public char dq() {
         if (size==0) {throw new IllegalArgumentException("Queue is empty");}
-        char t = a[front];
+        char t = q[front];
         front = (front+1)%cap;
         size-=1;
         return t;
     }
 
     public boolean isEmpty() {return size==0;}
-
-    public boolean isFull() {return size==cap;}
 
     public int getSize() {return size;}
 }
